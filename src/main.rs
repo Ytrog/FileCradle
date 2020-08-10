57e32ade85472fccc44c6f636bd8fe0a05adfb47
Ytrog <hisbrucker@gmail.com>
2020-08-10 17:39:12 +0200

    defined arguments using structopt

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,26 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
+    #[structopt(subcommand)]
+    action: Action,
+}
 
+#[derive(StructOpt)]
+enum Action {
+    Add {
+        #[structopt(parse(from_os_str))]
+        path: std::path::PathBuf,
+    },
+    Check {
+        #[structopt(parse(from_os_str))]
+        path: std::path::PathBuf,
+    },
+    List {},
 }
 
 fn main() {
     // get env variables
+    let args = Cli::from_args();
     // validate vars
     // determine what to do
     // implement adding file to hash list
